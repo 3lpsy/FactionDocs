c97ce947b67056941402a02fdab16b69df3130dc
Jared Haight <git@psattack.com>
2019-03-05 07:39:02 -0800

    initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Faction',
+  description: 'Documentation for the Faction C2 Framework',
+  themeConfig: {
+    logo: '/assets/img/faction-white-orange-fist-outline.png',
+    nav: [
+      { text: 'Documentation', link: '/docs/' },
+      { text: 'Marauder Shell', link: 'https://www.maraudershell.com'},
+      { text: 'Github', link: 'https://github.com/factionc2/'}
+    ],
+    sidebar: [
+      ['/docs/', 'Documentation'],
+      '/docs/components/',
+      '/docs/development/',
+      '/docs/operation/',
+    ],
+    lastUpdated: 'Last Updated'
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+# Documentation
+This is where the documentation goes. We'll talk about 
+
+## What is Faction
+
+## How Faction Works
+
+## Installing Faction
+
+## Using Faction
